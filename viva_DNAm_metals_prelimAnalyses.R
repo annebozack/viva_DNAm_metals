a31dbf0e59e949da7137a03a254810e928a6255a
annebozack <anne.bozack@gmail.com>
2020-12-14 11:23:53 -0500

    first commit

---
+++ viva_DNAm_metals_prelimAnalyses.R
@@ -0,0 +1,5 @@
+library(sas7bdat)
+
+pheno = read.sas7bdat('/Users/annebozack/Box/NIEHS-R01 ONES/Phenotype Data/anne_bozack_ac_120720.sas7bdat')
+
+load('/Users/annebozack/Box/DNA Methylation Data/ProjectVivaMethylationBetas_forCPrice_2020-06-10.RData')
